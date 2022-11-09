4918d21ed502053d6123e7c1201526bcd85a2922
Preeti <pree.p17@gmail.com>
2022-11-09 19:39:35 +0530

    updated homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My Awesome React Application
+          My Awesome GitHub page using React.
         </p>
         <a
           className="App-link"
